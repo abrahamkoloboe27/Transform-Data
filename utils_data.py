5e0364077dfa0ed73b246e926217ae093edf9221
abrahamkoloboe27 <114146395+abrahamkoloboe27@users.noreply.github.com>
2024-01-31 10:48:56 +0100

    Add files via upload

--- utils_data.py
+++ utils_data.py
@@ -11,16 +11,19 @@ def transform_data(df) :
     Valuee = []
     CumulValue = []
     Source = []
+    Code = []
     for row in df_np :
         Station.append(row[0])
         Date.append(str(int(row[2]))+"/"+str('{:02d}'.format(int(row[3])))+"/"+str('{:02d}'.format(int(row[4]))))
         Valuee.append(row[5])
         CumulValue.append(0)
+        Code.append([])
         Source.append("METEO-BENIN")
     data = pd.DataFrame({
         "Station" : Station,
         "Date" : Date,
         "Valuee" : Valuee,
+        "Codee" : Code, 
         "CumulValue" : CumulValue,
         "Source" : Source
         })
